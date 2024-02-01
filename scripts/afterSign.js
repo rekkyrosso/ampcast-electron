dfb40f1438d39bc0d568e0b1153bb95f2535d42d
rekky <rekky.rosso@gmail.com>
2024-02-01 04:39:20 +0000

    Add mac entitlements

--- scripts/afterSign.js
+++ scripts/afterSign.js
@@ -1,10 +1,11 @@
-const {notarize} = require('electron-notarize');
+const {notarize} = require('@electron/notarize');
 const {build} = require('../package.json');
 
 exports.default = async function (context) {
+    console.log(`afterSign: appOutDir=${context.appOutDir}`);
+
     if (process.platform === 'darwin') {
         // Notarize
-
         if (!('APPLE_ID' in process.env && 'APPLE_ID_PASSWORD' in process.env)) {
             console.warn(
                 'Skipping notarizing step. APPLE_ID and APPLE_ID_PASSWORD env variables must be set'
@@ -17,13 +18,14 @@ exports.default = async function (context) {
         const {appOutDir} = context;
         const appName = context.packager.appInfo.productFilename;
 
-        return notarize({
+        await notarize({
             appBundleId: build.appId,
             appPath: `${appOutDir}/${appName}.app`,
             appleId: process.env.APPLE_ID,
             appleIdPassword: process.env.APPLE_ID_PASSWORD,
-            teamId: process.env.APPLE_TEAM_ID,
         });
+
+        console.log('Notarization complete.');
     } else if (process.platform === 'win32') {
         // VMP sign via EVS
         const {execSync} = require('child_process');
