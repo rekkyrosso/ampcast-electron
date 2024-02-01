f5e5078242410b10d5aa7e55af1726bcf598bf4a
rekky <rekky.rosso@gmail.com>
2024-02-01 23:17:46 +0000

    Skip notarization

--- scripts/afterSign.js
+++ scripts/afterSign.js
@@ -3,8 +3,9 @@ const {build} = require('../package.json');
 
 exports.default = async function (context) {
     if (process.platform === 'darwin') {
-        // Notarize
-        if (!('APPLE_ID' in process.env && 'APPLE_ID_PASSWORD' in process.env)) {
+        const env = process.env;
+
+        if (!('APPLE_ID' in env && 'APPLE_ID_PASSWORD' in env)) {
             console.warn(
                 'Skipping notarizing step. APPLE_ID and APPLE_ID_PASSWORD env variables must be set'
             );
@@ -19,8 +20,8 @@ exports.default = async function (context) {
         await notarize({
             appBundleId: build.appId,
             appPath: `${appOutDir}/${appName}.app`,
-            appleId: process.env.APPLE_ID,
-            appleIdPassword: process.env.APPLE_ID_PASSWORD,
+            appleId: env.APPLE_ID,
+            appleIdPassword: env.APPLE_ID_PASSWORD
         });
 
         console.log('Notarization complete.');
